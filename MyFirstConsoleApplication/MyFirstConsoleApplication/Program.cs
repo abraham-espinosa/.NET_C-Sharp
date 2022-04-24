444e8f63e21066fc1fa7d208f046f4eba158e38d
abraham-1 <esp19005@byui.edu>
2022-04-23 20:30:52 -0500

    Implement GlazerApp and Person Class

--- MyFirstConsoleApplication/MyFirstConsoleApplication/Program.cs
+++ MyFirstConsoleApplication/MyFirstConsoleApplication/Program.cs
@@ -10,6 +10,35 @@ namespace MyFirstConsoleApplication
     {
         static void Main(string[] args)
         {
+            GetUserNameAndLocation();
+            ChristmasCountdown();
+            GlazerApp.RunExample();
+            Console.WriteLine("Press any key to continue.");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+
+        }
+        private static void GetUserNameAndLocation()
+        {
+            Person person = new Person();
+            Console.WriteLine("What is your name?");
+            person.name = Console.ReadLine();
+            Console.WriteLine($"Hi {person.name}! Where are you from?");
+            person.location = Console.ReadLine();
+            Console.WriteLine($"I have never been to {person.location}. I bet it is nice. Press any key to continue...");
+            Console.WriteLine("Press any key to continue.");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+        }
+
+        private static void ChristmasCountdown()
+        {
+            DateTime date = DateTime.Now;
+            Console.WriteLine($"Today's date is: {date.ToString("MMMM dd yyyy")}");
+            DateTime christmasDate = new DateTime(2022, 12, 25);
+            double daysForChristmas = (christmasDate - date).TotalDays;
+            Console.WriteLine($"There are {daysForChristmas:.} days until Christmas!");
+            Console.WriteLine("Press any key to continue.");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
         }
     }
 }
+
